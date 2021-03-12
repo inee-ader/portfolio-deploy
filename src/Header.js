fa4ef2778da425b9fd6c098fb36992fcea472c72
xoliio <xoliio@gmail.com>
2021-03-12 07:48:30 -0800

    changes order of header items

--- src/Header.js
+++ src/Header.js
@@ -46,12 +46,12 @@ class Header extends Component {
                         <Link className={`header__link ${this.props.active === 'about' ? 'header__about--active' : 'header__inactive'}`} to="/about">About</Link>
                     </li>
     
-                    <li >
-                        <Link className={`header__link ${this.props.active === 'other' ? 'header__other--active' : 'header__inactive'}`} to="/other">Rigamarole</Link>
-                    </li>
-                    
                     <li>
                         <Link className={`header__link ${this.props.active === 'pages' ? 'header__other--active' : 'header__inactive'}`} to='/pages'>Pages</Link>
+                    </li>
+
+                    <li >
+                        <Link className={`header__link ${this.props.active === 'other' ? 'header__other--active' : 'header__inactive'}`} to="/other">Rigamarole</Link>
                     </li>
                 </ul>
             </div>
